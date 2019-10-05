85880c40a924c7f786e0879ad2eec909196daa08
Vesa Karvonen <vesa.a.j.k@gmail.com>
2019-10-06 02:35:32 +0300

    Drop legacy files

--- c++17-no-rtti-no-exns.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT DEFINED CPPSM_EXCEPTIONS)
-  set(CPPSM_EXCEPTIONS OFF CACHE BOOL "Disable C++ exceptions")
-endif()
-
-if(NOT DEFINED CPPSM_RTTI)
-  set(CPPSM_RTTI OFF CACHE BOOL "Disable generation of RTTI information")
-endif()
-
-include(.cppsm/c++17.cmake)
